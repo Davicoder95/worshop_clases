01acbd66bee4d7646b7779d839238214d44d05aa
Davi <daviocassio95@gmail.com>
2024-08-30 16:53:27 -0500

    se inica repo

---
+++ src/main/java/com/example/worshop_clases/WorshopClasesApplication.java
@@ -0,0 +1,13 @@
+package com.example.worshop_clases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorshopClasesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorshopClasesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/worshop_clases/WorshopClasesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.worshop_clases;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorshopClasesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
